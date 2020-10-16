8ae95dbebcff05fa995a2746cc5c09bf96a29d19
kenvenner <40737866+kenvenner@users.noreply.github.com>
2020-10-16 05:59:14 -0700

    Add files via upload

---
+++ epicstatus.py
@@ -0,0 +1,53 @@
+# may comment out in the future
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+from atlassian import Jira
+
+url= 'your-url',
+username= 'your-account'
+token='your-token-or-password'
+ticket='your-ticket-number'
+
+
+def jira_find_field( jira, fld, dumpall=False ):
+    results = jira.get_all_fields()
+    if dumpall:
+        pp.pprint(results)
+        return
+    
+    for result in results:
+        if fld == result['name']:
+            pp.pprint(result)
+
+
+jira = Jira(
+    url=url,
+    username=username,
+    password=token)
+
+print('Field information in our instance')
+jirafld = 'Epic Status'
+jira_find_field( jira, jirafld )
+# try different structures
+jira_epic_done_field_formats = [
+    { "customfield_10010" : [{"value" : "Done",}],},
+    { "customfield_10010" : {"value" : "Done",},},
+    { "customfield_10010" : "Done" },
+    { "fields" : { "customfield_10010" : {"value" : "Done",},},},
+    { "fields" : { "customfield_10006" : {"value" : "Done",},},},
+    { "customfield_10006" : {"value" : "Done",},},
+]
+print('-'*80)
+print('Current value for this field')
+result = jira.issue_field_value( ticket, "customfield_10010" )
+print(result)
+for jira_epic_done_fields in jira_epic_done_field_formats:
+    print('-'*80)
+    print('update json payload:')
+    pp.pprint(jira_epic_done_fields)
+    try:
+        result = jira.update_issue_field( ticket, jira_epic_done_fields )
+        print('epic status updated')
+    except Exception as e:
+        print(str(e))
